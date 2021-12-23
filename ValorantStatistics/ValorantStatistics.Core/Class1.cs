dd701ae6b2a03c8db83ae041e8ae79dbb01d77c2
Luis Henrique Nicoleli de Mello <lhnmello@hotmail.com>
2021-12-23 15:09:56 -0300

    Commit inicial

---
+++ ValorantStatistics/ValorantStatistics.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ValorantStatistics.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ ValorantStatistics/ValorantStatistics.Domain/Account/AccountDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ValorantStatistics.Domain.Account
+{
+    public class AccountDto
+    {
+        public string Puuid { get; set; }
+        public string GameName { get; set; }
+        public string Tagline { get; set; }
+    }
+}
